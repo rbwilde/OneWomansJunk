c482684119be214064671826cc9376570f55c8a2
Raina Koren <rainakoren@gmail.com>
2021-05-18 15:25:16 -0400

    Layout done, Adding Shopping cart

---
+++ owj/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import { Products, Navbar } from './components'
+import { commerce } from './lib/commerce'
+
+const App = () => {
+
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState({})
+
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list();
+
+        setProducts(data);
+    }
+
+    const fetchCart = async () => {
+        setCart(await commerce.cart.retrieve())
+    }
+
+    const handleAddToCart = async (productId, quantity) => {
+        const item = await commerce.cart.add(productId, quantity);
+
+        setCart(item.cart)
+    }
+
+    useEffect( () => {
+        fetchProducts();
+        fetchCart();
+    }, []);
+
+    console.log(cart)
+ 
+
+    return (
+        <div>
+            <Navbar />
+            <Products products={products} />
+        </div>
+    )
+}
+
+export default App
